bfafcc4a954b437a24b385fb64b8f32b96076595
admintrsp <admintrsp@gmail.com>
2026-07-04 11:15:17 -0600

    Add Apps Script environment variable

--- api/partner.js
+++ api/partner.js
@@ -1,16 +1,31 @@
 export default async function handler(req, res) {
-
   if (req.method !== "POST") {
     return res.status(405).json({
       error: "Method not allowed",
     });
   }
 
-  console.log(req.body);
+  try {
+const response = await fetch(
+  process.env.GOOGLE_APPS_SCRIPT_URL,
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req.body),
+  }
+);
+    const data = await response.json();
 
-  return res.status(200).json({
-    success: true,
-    received: req.body,
-  });
+    return res.status(200).json(data);
 
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
 }
